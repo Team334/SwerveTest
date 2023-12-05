4e4b5604526767744c4827d5bbcdfe97e0ae4da3
PGgit08 <peter.mty8@gmail.com>
2023-12-05 14:02:47 -0500

    started adding module state boilerplate

--- src/main/java/frc/robot/utils/SwerveModule.java
+++ src/main/java/frc/robot/utils/SwerveModule.java
@@ -6,11 +6,16 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.Constants;
 
 public class SwerveModule {
     private final TalonFX _driveMotor;
     private final TalonFX _rotationMotor;
+
+    private final PIDController _rotationController;
 
     private final CANCoder _encoder;
 
@@ -22,6 +27,8 @@ public class SwerveModule {
 
         _encoder.configMagnetOffset(angleOffset, Constants.CAN.CAN_TIMEOUT);
         _encoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180, Constants.CAN.CAN_TIMEOUT);
+
+        _rotationController = new PIDController(0, 0, 0);
 
         TalonFXConfig.configureFalcon(_driveMotor);
         TalonFXConfig.configureFalcon(_rotationMotor);
@@ -35,7 +42,20 @@ public class SwerveModule {
         _rotationMotor.set(TalonFXControlMode.PercentOutput, speed);
     }
 
+    public double getDriveVelocity() {
+        return _driveMotor.getSelectedSensorVelocity();
+    }
+
     public double getAngle() {
         return _encoder.getAbsolutePosition();
+    }
+
+    public void setState(SwerveModuleState state) {
+
+    }
+
+    public SwerveModuleState getState() {
+        // TODO: Make this return a value and use radians?
+        return new SwerveModuleState();
     }
 }
